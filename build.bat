b16cee1c9485ffa4b99ca48dd2b6012fbcaef56a
Amtoft <lukas@amtoft.eu>
2021-01-19 19:26:00 +0100

    Sleep is now 40 in bat aswell

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 call mvn clean package -Dquarkus.package.type=uber-jar -Dmaven.test.skip=true
 call docker build -t reports .
 call docker-compose up -d --build
-timeout /t 10
+timeout /t 40
 call mvn test
 call docker-compose down
